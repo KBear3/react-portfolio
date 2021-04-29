b8827e584bac3ccaa1b3209545f12e3f3e748762
KBear3 <73043478+KBear3@users.noreply.github.com>
2021-04-29 00:53:55 -0400

    Add files via upload

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import "../styles/Header.css";
+
+function Header () {
+    return (
+      <div className="header">
+        <h1>Employee Directory</h1>
+        <p>Use search boxes for a direct result, or sort each column by arrows.</p>
+      </div>
+    )
+  }
+
+export default Header;
